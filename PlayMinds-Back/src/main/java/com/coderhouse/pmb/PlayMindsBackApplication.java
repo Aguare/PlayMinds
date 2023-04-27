49b19f1eae25bf54dc7b87a6e2915334cef86af8
Aguare <marcosaguare201832069@cunoc.edu.gt>
2023-04-26 20:11:31 -0600

    Add Backend Project

---
+++ PlayMinds-Back/src/main/java/com/coderhouse/pmb/PlayMindsBackApplication.java
@@ -0,0 +1,13 @@
+package com.coderhouse.pmb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayMindsBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlayMindsBackApplication.class, args);
+    }
+
+}
