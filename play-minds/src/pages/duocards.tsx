ad7098ae7f8e2807a0ac272fef291284ef43d3a0
Emilio <emiliomalrod23@gmail.com>
2023-05-05 01:02:39 -0600

    Ya casi funciona el DuoCards

--- play-minds/src/pages/duocards.tsx
+++ play-minds/src/pages/duocards.tsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react'
 import { useSpring, animated } from 'react-spring'
 import { useDrag } from 'react-use-gesture'
-
 import NavBar from '../components/navbar'
 
 interface Card {
@@ -16,64 +15,134 @@ const Duocards = () => {
   const [cards, setCards] = useState<Card[]>([
     {
       id: 1,
-      name: '¿Cuál es la capital de Francia?',
+      name: '¿La capital de Francia es: ?',
       image: 'https://via.placeholder.com/150',
       description: 'París',
       correct: true,
     },
     {
       id: 2,
-      name: '¿Cuál es el río más largo del mundo?',
+      name: '¿El río más largo del mundo es: ?',
       image: 'https://via.placeholder.com/150',
       description: 'El río Amazonas',
       correct: false,
     },
+    {
+      id: 3,
+      name: '¿Cuál es el elemento más abundante en la Tierra?',
+      image: 'https://via.placeholder.com/150',
+      description: 'El oxígeno',
+      correct: false,
+    },
+    {
+      id: 4,
+      name: '¿Quién escribió la novela "Cien años de soledad"?',
+      image: 'https://via.placeholder.com/150',
+      description: 'Gabriel García Márquez',
+      correct: true,
+    },
+    {
+      id: 5,
+      name: '¿En qué año comenzó la Segunda Guerra Mundial?',
+      image: 'https://via.placeholder.com/150',
+      description: '1939',
+      correct: true,
+    },
+    {
+      id: 6,
+      name: '¿Qué país tiene la población más grande del mundo?',
+      image: 'https://via.placeholder.com/150',
+      description: 'China',
+      correct: true,
+    },
+    {
+      id: 7,
+      name: '¿Quién fue el primer hombre en pisar la luna?',
+      image: 'https://via.placeholder.com/150',
+      description: 'Neil Armstrong',
+      correct: true,
+    },
+    {
+      id: 8,
+      name: '¿Quién pintó la obra "La noche estrellada"?',
+      image: 'https://via.placeholder.com/150',
+      description: 'Vincent van Gogh',
+      correct: true,
+    },
+    {
+      id: 9,
+      name: '¿Cuál es el nombre del continente más grande del mundo?',
+      image: 'https://via.placeholder.com/150',
+      description: 'Asia',
+      correct: true,
+    },
+    {
+      id: 10,
+      name: '¿Qué animal representa al signo zodiacal de Leo?',
+      image: 'https://via.placeholder.com/150',
+      description: 'El león',
+      correct: true,
+    },
   ])
-
   const [currentCardIndex, setCurrentCardIndex] = useState<number>(0)
   const [answeredCards, setAnsweredCards] = useState<Card[]>([])
-  const [isCorrect, setIsCorrect] = useState<boolean>(false)
+  const [isCorrect, setIsCorrect] = useState<boolean | undefined>(undefined)
   const [isAnswered, setIsAnswered] = useState<boolean>(false)
+  const [gameOver, setGameOver] = useState<boolean>(false)
+  const [correctAnswers, setCorrectAnswers] = useState<number>(0)
 
   const [props, set] = useSpring(() => ({
     x: 0,
     y: 0,
     rotate: 0,
     scale: 1,
-    config: { mass: 5, tension: 500, friction: 80 },
+    config: { mass: 5, tension: 500, friction: 120 },
   }))
 
   useEffect(() => {
     setCurrentCardIndex(0)
     setAnsweredCards([])
+    setGameOver(false)
+    setCorrectAnswers(0) // Reiniciar el contador al comenzar un nuevo juego
   }, [cards])
 
   const handleSwipe = (direction: string) => {
     if (!isAnswered) {
       const currentCard = cards[currentCardIndex]
-      if (direction === 'right') {
-        setIsCorrect(currentCard.correct)
-      } else if (direction === 'left') {
-        setIsCorrect(!currentCard.correct)
-      }
+      const newAnsweredCards = [...answeredCards, currentCard]
+      const remainingCards = cards.filter(
+        (card) => !newAnsweredCards.includes(card),
+      )
+      const isCorrect =
+        direction === 'right' ? currentCard.correct : !currentCard.correct
+      console.log(`Usuario eligió ${direction}`)
+      console.log(
+        `La respuesta correcta es ${currentCard.correct ? 'right' : 'left'}`,
+      )
       set({
         x: direction === 'right' ? 300 : -300,
         rotate: direction === 'right' ? 45 : -45,
         scale: 1.2,
       })
       setIsAnswered(true)
-      if (!answeredCards.includes(currentCard)) {
-        setAnsweredCards([...answeredCards, currentCard])
+
+      if (isCorrect) {
+        setCorrectAnswers((prev) => prev + 1) // Actualizar el contador si se responde correctamente
       }
-      setTimeout(() => {
-        const remainingCards = cards.filter(
-          (card) => !answeredCards.includes(card),
+
+      if (remainingCards.length > 0) {
+        const newIndex = Math.min(
+          currentCardIndex + 1,
+          remainingCards.length - 1,
         )
-        if (remainingCards.length > 0) {
-          setCurrentCardIndex((currentCardIndex + 1) % remainingCards.length)
-        } else {
-          alert('¡Has completado todas las cartas!')
-        }
+        setCurrentCardIndex(newIndex)
+        setCards([...remainingCards])
+        setAnsweredCards(newAnsweredCards)
+      } else {
+        setGameOver(true)
+      }
+
+      setTimeout(() => {
         setIsAnswered(false)
         set({ x: 0, rotate: 0, scale: 1 })
       }, 500)
@@ -89,34 +158,39 @@ const Duocards = () => {
   return (
     <div className="bg-gray-100 min-h-screen">
       <NavBar />
-      <div className="flex justify-center items-center h-full">
-        <animated.div
-          className="card"
-          {...bind()}
-          style={{
-            transform: props.x
-              .interpolate({
-                range: [-300, 0, 300],
-                output: [-45, 0, 45],
-              })
-              .interpolate((x) => `translate3d(${x}px,0,0)`),
-          }}
-        >
-          <h1 className="card-title">{cards[currentCardIndex].name}</h1>
-          <img
-            className="card-image"
-            src={cards[currentCardIndex].image}
-            alt={cards[currentCardIndex].name}
-          />
-          <p className="card-description">
-            {cards[currentCardIndex].description}
-          </p>
-          {isAnswered && (
-            <p className="card-feedback">
-              {isCorrect ? '¡Correcto!' : 'Incorrecto.'}
+      <div className="grid place-content-center content-center h-full">
+        {gameOver ? (
+          <div className="game-over">
+            <h2>¡Juego terminado!</h2>
+            <p>Todas las cartas han sido respondidas.</p>
+            <div className="correct-answers">
+              Respuestas correctas: {correctAnswers}/{cards.length}
+            </div>
+          </div>
+        ) : (
+          <animated.div
+            className="card grid grid-cols-1 p-2 gap-3 place-items-center"
+            {...bind()}
+            style={{
+              transform: props.x
+                .interpolate({
+                  range: [-300, 0, 300],
+                  output: [-45, 0, 45],
+                })
+                .interpolate((x) => `translate3d(${x}px,0,0)`),
+            }}
+          >
+            <h1 className="card-title">{cards[currentCardIndex].name}</h1>
+            <img
+              className="card-image "
+              src={cards[currentCardIndex].image}
+              alt={cards[currentCardIndex].name}
+            />
+            <p className="card-description">
+              {cards[currentCardIndex].description}
             </p>
-          )}
-        </animated.div>
+          </animated.div>
+        )}
       </div>
     </div>
   )
