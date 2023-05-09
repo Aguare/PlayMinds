73d0e93fbd31fbe1dc0a346f5f5f17594303c150
9601dani <erickmorales201930699@cunoc.edu.gt>
2023-05-09 07:41:12 -0600

    test DAO user

--- PlayMinds-Back/src/main/java/com/coderhouse/Controllers/UserCTRL.java
+++ PlayMinds-Back/src/main/java/com/coderhouse/Controllers/UserCTRL.java
@@ -5,16 +5,21 @@ import com.coderhouse.Entitys.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Iterator;
 import java.util.Optional;
 
 @RestController
 @Slf4j
-@CrossOrigin(origins = "http://localhost:300")
+@CrossOrigin(origins = "http://localhost:3000")
 public class UserCTRL{
 
     @Autowired
     private UserDAO user;
 
+    /**
+     * @return
+     */
     @GetMapping("/GetUser")
     public User getUser(String email){
         Optional<User> userReceipt = this.user.findById(email);
@@ -23,5 +28,38 @@ public class UserCTRL{
         }else{
             return null;
         }
+    }
+    /**
+     * @return
+     */
+    @GetMapping("/GetAll")
+    public Iterable<User> getAllUser(){
+         Iterable<User> usersReceipt= this.user.findAll();
+         Iterator<User> iterator = usersReceipt.iterator();
+            if (iterator.hasNext()) {
+                return usersReceipt;
+            } else {
+                return null;
+            }     
+    }
+    
+    /**
+     * @return
+     */
+    @GetMapping("/SaveUser")
+    public User saveUser(User user){
+        User userReceipt= this.user.save(user);
+        if(this.user.findById(userReceipt.getEmail()).isPresent()){
+            return userReceipt;
+        }else{
+            return null;
+        }
+    }
+    /**
+     * @return
+     */
+    @GetMapping("/DeleteUser")
+    public void deleteUser(String email){
+        this.user.deleteById(email);
     }
 }
