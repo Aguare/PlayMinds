d273d768cb80cc9eeec733d562e8086bc5c2754f
9601dani <erickmorales201930699@cunoc.edu.gt>
2023-05-23 00:41:35 -0600

    get hanged

--- play-minds/src/pages/game/ahorcado.tsx
+++ play-minds/src/pages/game/ahorcado.tsx
@@ -5,27 +5,64 @@ import NavBar from '@/components/navbar'
 import Table from '@/components/table'
 import Comments from '@/components/comment'
 import NewComment from '@/components/newComment'
-
+import { useRouter } from 'next/router'
+import { HangedGame } from '@/models/Entitys/Assistant/HangedGame'
+import { Game } from '@/models/Entitys/Game'
+import { User } from '@/models/Entitys/User'
+import { Request } from '@/helpers/requests'
+import axios from 'axios'
+import { Phrase } from '@/models/Entitys/Phrase'
 function Hangman() {
-  interface HangmanWord {
-    word: string
-    hint: string
-  }
-  const words: HangmanWord[] = [
-    { word: 'programacion', hint: 'Acción de escribir código' },
-    { word: 'javascript', hint: 'Lenguaje de programación popular en la web' },
+  var user= new User("","","","",0)
+  const route = useRouter()
+  const { id } = route.query
+  const [hangedGame, setHangedGame] = useState<HangedGame>(
+    new HangedGame(
+      new Game('default', '', '', '', 0, new User('', '', '', '', 0)),
+      [],
+    ),
+  )
+  const [words,setWords] = useState<Phrase[]> ([
+    { phrase: 'programacion', hint: 'Acción de escribir código' },
+    { phrase: 'javascript', hint: 'Lenguaje de programación popular en la web' },
     {
-      word: 'react',
+      phrase: 'react',
       hint: 'Biblioteca de JavaScript para construir interfaces de usuario',
     },
-  ]
+  ])
+
+  if (
+    id != "default" &&
+    id != undefined &&
+    hangedGame.game.id_game == "default"
+  ) {
+    axios
+      .get(Request.SERVER + "/Games/GetHangedGame?id_game=" + id, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then(function (response) {
+        let tmp = localStorage.getItem("user");
+        if (tmp) {
+          user = JSON.parse(tmp);
+        }
+        setHangedGame(response.data);
+       hangedGame.phrases = response.data.phrases;
+        setWords(hangedGame.phrases);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }
+
   const [cont, setCont] = useState(0)
   function getRandomWord() {
     /* const randomIndex = Math.floor(Math.random() * words.length); */
     setCont(cont + 1)
     return words[cont]
   }
-  const [word, setWord] = useState<HangmanWord>({ word: '', hint: '' })
+  const [word, setWord] = useState<Phrase>({ phrase: '', hint: '' })
   const [hiddenWord, setHiddenWord] = useState('')
   const [attempts, setAttempts] = useState(0)
   const [lose, setLose] = useState(false)
@@ -39,7 +76,7 @@ function Hangman() {
   useEffect(() => {
     const newWord = getRandomWord()
     setWord(newWord)
-    setHiddenWord('_ '.repeat(newWord.word.length))
+    setHiddenWord('_ '.repeat(newWord.phrase.length))
     setAttempts(0)
     setLose(false)
     setWon(false)
@@ -67,8 +104,8 @@ function Hangman() {
   useEffect(() => {
     if (
       hiddenWord &&
-      hiddenWord.split(' ').join('') === word.word &&
-      word.word !== ''
+      hiddenWord.split(' ').join('') === word.phrase &&
+      word.phrase !== ''
     ) {
       setWon(true)
     }
@@ -85,14 +122,14 @@ function Hangman() {
     if (lose) return
     if (won) return
     console.log(letter)
-    if (!word.word.includes(letter.toLowerCase())) {
+    if (!word.phrase.includes(letter.toLowerCase())) {
       setAttempts(Math.min(attempts + 1, 9))
       return
     }
 
     const hiddenWordArray = hiddenWord.split(' ')
-    for (let i = 0; i < word.word.length; i++) {
-      if (word.word[i] === letter.toLowerCase()) {
+    for (let i = 0; i < word.phrase.length; i++) {
+      if (word.phrase[i] === letter.toLowerCase()) {
         hiddenWordArray[i] = letter.toLowerCase()
       }
     }
@@ -102,7 +139,7 @@ function Hangman() {
   const newGame = () => {
     const newWord = getRandomWord()
     setWord(newWord)
-    setHiddenWord('_ '.repeat(newWord.word.length))
+    setHiddenWord('_ '.repeat(newWord.phrase.length))
     setAttempts(0)
     setLose(false)
     setWon(false)
@@ -146,7 +183,7 @@ function Hangman() {
               {lose && siguiente ? (
                 <div className="grid grid-cols-1 p-2 gap-3 place-items-center">
                   <h2 className="font-semibold text-[#D21312] text-2xl">
-                    ¡Perdiste! la palabra era: {word.word}
+                    ¡Perdiste! la palabra era: {word.phrase}
                   </h2>
                   <button
                     className="bg-[#205375] p-2 font-semibold rounded-lg text-[white] hover:bg-[#F66B0E]"
