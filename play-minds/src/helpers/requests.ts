5431d89f771c2d8712127987b8057fcddea1a489
9601dani <erickmorales201930699@cunoc.edu.gt>
2023-05-22 18:09:31 -0600

    changes path

--- play-minds/src/helpers/requests.ts
+++ play-minds/src/helpers/requests.ts
@@ -1,5 +1,6 @@
 export enum Request {
-  SERVER = "http://localhost:8080",
+  SERVER = "http://localhost:8080/PlayMind",
+  SERVER_API = "http://localhost:8080",
   GET_ALL_GAMES = SERVER + "/Games/GetAllGames",
   REGISTER_HANGED_GAME = SERVER + "/Games/RegisterHangedGame",
   UPLOAD_DUO_CARD = SERVER + "/Files/upload",
@@ -8,4 +9,5 @@ export enum Request {
   REGISTER_MEMORY_GAME = SERVER + "/Games/RegisterMemoryGame",
   REGISTER_QUIZ_GAME = SERVER + "/Games/RegisterQuizGame",
   REGISTER_USER = SERVER + "/Users/RegisterUser",
+  SERVER_IMAGE = SERVER_API +"/images"
 }
